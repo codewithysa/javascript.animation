5d561225d6dfc97fd0c20c43df7f66c0676865e6
isabel <mgatongay253@yahoo.com>
2021-11-18 01:55:06 +0800

    animation update

--- animation.js
+++ animation.js
@@ -8,13 +8,13 @@ function myFunction(){
     const item3 = document.getElementsByClassName('click-me');
     const items = item3[0]
 
-    console.log(items)
 
     setTimeout(function(){
        item1.style.transition = '2s ease';
        item1.style.opacity = '1';
        item1.style.fontSize = '40px';
-       item1.style.transform = 'translateY(50%)'
+       item1.style.transform = 'translateY(80%)'
+       item1.style.paddingTop = '50px';
     }, 100)
 
     setTimeout(function(){
@@ -24,17 +24,39 @@ function myFunction(){
         item2.style.opacity = '1';
         item2.style.transform = 'translateY(50%)'
         item2.style.fontSize = '40px';
-    }, 2000)
+    }, 3500)
 
     setTimeout(function(){
       items.style.opacity = '1';
       items.style.transform = '2s ease';
       items.style.animation = '2s click linear infinite';
       items.style.cursor = 'pointer';
-    }, 5000)
+      item1.style.paddingTop = '10px';
+    }, 6000)
+
 
     items.addEventListener('click', function(){
        
+      const pickColor = document.querySelector('.pick-color');
+      const gridColor = document.querySelectorAll('.grid-colors');
+
+      for(i = 0; i < gridColor.length; i++){
+        gridColor[i].style.opacity = '1';
+        gridColor[i].style.transform = 'translateY(-80%)';
+        gridColor[i].style.transition = '3s ease';
+        gridColor[i].style.marginLeft = '10px';
+        gridColor[i].style.marginTop = '10px';
+        item2.style.opacity = '0';
+        items.style.color = 'transparent';
+      }
+      
+  
+      pickColor.style.opacity = '1'
+      pickColor.style.transition = '3s ease';
+      pickColor.style.transform = 'rotateY(360deg)';
+      pickColor.style.animation = '5s pickColor linear infinite'
+
     })
 }
+
 
